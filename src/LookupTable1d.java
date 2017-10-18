7f67a55dae994d8b3cb289957bc8c0f84eefd831
FMT <t.f.j.velthausz@student.utwente.nl>
2017-10-18 17:19:46 +0200

    Eddited more JML errors

--- src/LookupTable1d.java
+++ src/LookupTable1d.java
@@ -46,11 +46,11 @@ class LookupTable1d {
 			//REPORT THIS IS WRONG, needs to be 0.f * vn
 			v = v + f;
 		}
-		// ASSERTION(S)
+		// //@ assertION(S)
 		if(i == lookupValues.length) {
-			assert v == lookupValues[i];
+			//@ assert v == lookupValues[i];
 		}else {
-			assert v == lookupValues[i] + (f * lookupValues[i+1]) / 100;
+			//@ assert v == lookupValues[i] + (f * lookupValues[i+1]) / 100;
 		}
 		// (note, what you want to check here would normally
 		//  be part of the postcondition, but would produce a very
